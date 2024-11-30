7cd2cb84372f803cd771340c7454cc6a145becdd
mister-nothing00 <francescodavidedivita@gmail.com>
2024-11-30 12:32:25 +0100

    Create card slider component e card component

--- client/src/pages/Netflix.jsx
+++ client/src/pages/Netflix.jsx
@@ -4,30 +4,37 @@ import Navbar from "../components/ui/personal/Navbar";
 import { Box } from "@chakra-ui/react";
 import Hero from "../components/ui/personal/Hero";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { getGenres } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
+import Slider from "../components/ui/personal/Slider";
 
 export default function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const navigate=useNavigate();
-  const dispatch=useDispatch();
+  const navigate = useNavigate();
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const movies= useSelector((state)=> state.netflix.movies)
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(getGenres())
-  }, [])
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
 
+  useEffect(() => {
+    if (genresLoaded) dispatch(fetchMovies({ type: "all" }));
+  }, []);
 
   window.onscroll = () => {
     setIsScrolled(window.pageXOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
+
+ 
   return (
     <>
-      <Box position={"relative"} bgColor={"blackAlpha.950"} height={"100vh"}  >
+      <Box  bgColor={"blackAlpha.950"} height={"auto"}>
         <Navbar isScrolled={isScrolled} />
-        <Hero/>
-       
-
+        <Hero />
+        <Slider movies={movies}/>
       </Box>
     </>
   );
